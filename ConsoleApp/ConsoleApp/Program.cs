2d8d7053dbeddf822187dc35b7b2e392d795ee90
Rofiq02 <abdulrafiqyusufa84@gmail.com>
2018-08-31 02:19:49 +0700

    Tambahan

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -19,25 +19,45 @@ namespace ConsoleApp
             OnProgram.BentukKotak (1, 7, 118, 9);
             OnProgram.BentukKotak (40, 7, 118, 9);
             OnProgram.BentukKotak(80, 7, 118, 9);
-            OnProgram.BentukKotak(0, 11, 119, 43);
+            OnProgram.BentukKotak(0, 11, 119, 30);
 
             String judul = " Wearnes Education Center ";
             OnProgram.RataTengah((120 - judul.Length) / 2, 2, judul);
             string subjudul = " IK 1 - 2018 ";
             OnProgram.RataTengah((120 - subjudul.Length) / 2, 3, subjudul);
 
-            /*
+            
             string[] menu = new string[3];
             menu[0] = " SISWA ";
             menu[1] = " DOSEN ";
             menu[2] = " MATA KULIAH ";
-            
 
             for (int i = 0; i < 3; i++)
             {
-                OnProgram.RataTengah(2, 7 + i, menu[i]);
+                OnProgram.RataTengah(18 + (i * 38), 8, menu[i]);
             }
-            */
+
+            int pilihan = 0;
+            OnProgram.WarnaTampilan(18, 8, menu [pilihan], ConsoleColor.Black, ConsoleColor.Blue);
+
+            ConsoleKeyInfo tombol;
+            do
+           {
+             tombol = Console.ReadKey (true);
+
+                if (tombol.Key == ConsoleKey.RightArrow)
+                {
+                    OnProgram.WarnaTampilan (18, 8 + pilihan, menu [pilihan], ConsoleColor.White, ConsoleColor.Black);
+                    if (tombol.Key == ConsoleKey.RightArrow)
+                    {
+                    pilihan++;
+                    }
+                    OnProgram.WarnaTampilan (18, 8 + pilihan, menu [pilihan], ConsoleColor.Black, ConsoleColor.Blue);
+                }
+
+           }while(tombol.Key != ConsoleKey.Enter);
+
+            
 
 
             Console.ReadKey();
