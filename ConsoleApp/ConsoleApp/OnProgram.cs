2a1447f1598831fde55c4b1cc1e3ecc075703aaa
Rofiq02 <abdulrafiqyusufa84@gmail.com>
2018-08-31 01:12:23 +0700

    kirim

---
+++ ConsoleApp/ConsoleApp/OnProgram.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class OnProgram
+    {
+        static public void BentukKotak (int kiri, int atas, int kanan, int bawah)
+        {
+            Console.SetCursorPosition (kiri,atas);
+            Console.Write ("┌");
+
+            for (int i = kiri + 1; i <= kanan - 1; i++)
+            {
+                Console.SetCursorPosition (i, atas);
+                Console.Write("─");
+            }
+
+            Console.SetCursorPosition (kanan, atas);
+            Console.Write("┐");
+
+            for (int i = atas + 1; i <= bawah - 1; i++)
+            {
+                Console.SetCursorPosition(kanan, i);
+                Console.Write("│");
+            }
+
+            Console.SetCursorPosition (kanan, bawah);
+            Console.Write("┘");
+
+            for (int i = kanan - 1; i >= kiri + 1; i--)
+            {
+                Console.SetCursorPosition(i, bawah);
+                Console.Write("─");
+            }
+
+            Console.SetCursorPosition(kiri, bawah);
+            Console.Write("└");
+
+            for (int i = bawah - 1; i >= atas + 1; i--)
+            {
+                Console.SetCursorPosition(kiri, i);
+                Console.Write("│");
+            }
+
+        }//Tutup
+
+
+        static public void WarnaTampilan(int kiri, int atas, string teks, ConsoleColor WarnaTeks,
+            ConsoleColor WarnaBackground)
+        {
+            Console.SetCursorPosition(kiri, atas);
+            Console.ForegroundColor = WarnaTeks;
+            Console.BackgroundColor = WarnaBackground;
+            Console.Write(teks);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+
+
+       static public void RataTengah(int kiri, int atas, string teks)
+        {
+  
+            Console.SetCursorPosition(kiri, atas);
+            Console.Write(teks);
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+
+            OnProgram.BentukKotak (0, 0, 119, 5);
+            OnProgram.BentukKotak (0, 6, 119, 10);
+            OnProgram.BentukKotak (1, 7, 118, 9);
+            OnProgram.BentukKotak (40, 7, 118, 9);
+            OnProgram.BentukKotak(80, 7, 118, 9);
+            OnProgram.BentukKotak(0, 11, 119, 43);
+
+            String judul = " Wearnes Education Center ";
+            OnProgram.RataTengah((120 - judul.Length) / 2, 2, judul);
+            string subjudul = " IK 1 - 2018 ";
+            OnProgram.RataTengah((120 - subjudul.Length) / 2, 3, subjudul);
+
+            /*
+            string[] menu = new string[3];
+            menu[0] = " SISWA ";
+            menu[1] = " DOSEN ";
+            menu[2] = " MATA KULIAH ";
+            
+
+            for (int i = 0; i < 3; i++)
+            {
+                OnProgram.RataTengah(2, 7 + i, menu[i]);
+            }
+            */
+
+
+            Console.ReadKey();
+        }
+    }
+}
